0aca447208a43c6503f8e7d10be283d8316c6aad
sam-yng <young142001@hotmail.com>
2023-07-17 09:19:45 +1000

    setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import classNames from "classnames";
+import React from "react";
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <div className={classNames("text-4xl")}>hello</div>
+    </>
+  );
+};
